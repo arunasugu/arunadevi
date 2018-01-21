679e4c2eb6db5977ffba8e3569f04822dec27801
arunasugu <35652990+arunasugu@users.noreply.github.com>
2018-01-21 16:41:16 +0530

    Update and rename bha.c to rah.c

---
+++ rah.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a,z;
+clrscr();
+  printf("enter the value of c");
+  scanf("%c",a);
+if(a<=z)
+{
+printf("alphabet");
+}
+else
+{
+printf("not");
+}
+getch();
+}
